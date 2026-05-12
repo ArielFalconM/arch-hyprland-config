95e58dbe417b55cc70569dd804296d479278e939
MrMermaid <ariel.falcon.m@gmail.com>
2026-05-12 01:19:44 -0300

    refactor: script manejo notificaciones spotify mejorado

--- scripts/spotify_notify.sh
+++ scripts/spotify_notify.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+last=""
 # Monitoreamos cambios en Spotify usando un separador único (|)
 playerctl --player=spotify metadata --format '{{title}}|{{artist}}' --follow | while read -r line; do
-    if [ -n "$line" ]; then
+    if [ -n "$line" ] && [ "$line" != "$last" ]; then
+        last="$line"
         # Separamos la info: f1 es título (canción), f2 es artista
         title=$(echo "$line" | cut -d'|' -f1)
         artist=$(echo "$line" | cut -d'|' -f2)
 
         # Extraemos la URL de la carátula
         img_url=$(playerctl --player=spotify metadata mpris:artUrl)
-        
+
         # Descargamos la imagen para que notify-send la procese
         curl -s "$img_url" -o /tmp/spotify_cover.png
-        
+
         # Enviamos: canción como título, artista como cuerpo
         notify-send -u normal -i /tmp/spotify_cover.png "$title" "$artist"
     fi
